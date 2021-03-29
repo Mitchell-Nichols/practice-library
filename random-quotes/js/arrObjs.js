3fe3b3a05c9a0236fe5dd512bbebd12da9a134f4
mifch21 <mln6023@gmail.com>
2021-03-29 18:54:27 -0500

    Updated codes and delete unnecessary files

--- random-quotes/js/arrObjs.js
+++
@@ -1,7 +0,0 @@
-const quotes = [
-{quote: "Life is never fair, and perhaps it is a good thing for most of us that it is not.", author: "Oscar Wilde"},
-{quote: "The only impossible journey is the one you never begin.", author: "Tony Robbins"},
-{quote: "You only live once, but if you do it right, once is enough.", author: "Mae West"},
-{quote: "Life is really simple, but we insist on making it complicated.", author: "Confucius"},
-{quote: "The way to get started is to quit talking and begin doing.", author: "Walt Disney"}
-];
--- random-quotes/js/script.js
+++ random-quotes/js/script.js
@@ -1,52 +1,52 @@
 /******************************************
 Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
+Author: Mitchell Nichols
+Complete Date: March 29, 2021
 ******************************************/
 
-// For assistance:
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/***
- * `quotes` array
-***/
+// Storing my 5 selected quotes.
 const quotes = [
 {quote: "Life is never fair, and perhaps it is a good thing for most of us that it is not.", source: "Oscar Wilde"},
 {quote: "The only impossible journey is the one you never begin.", source: "Tony Robbins"},
-{quote: "You only live once, but if you do it right, once is enough.", source: "Mae West"},
+{quote: "You only live once, but if you do it right, once is enough.", source: "Mae West", year:"August 17, 1893"},
 {quote: "Life is really simple, but we insist on making it complicated.", source: "Confucius"},
-{quote: "The way to get started is to quit talking and begin doing.", source: "Walt Disney"}
+{quote: "The way to get started is to quit talking and begin doing.", source: "Walt Disney", citation:"google.com"}
 ];
 
 
-
-/***
- * `getRandomQuote` function
-***/
-let strPrint = ``;
+//Run the random function to random select a quote from quotes object. Return the selected quote.
 function getRandomQuote (){
-
-
-    let numR = Math.floor(Math.random() * quotes.length);
-    let selected = quotes[numR];
-
-    strPrint += `
-      <h1>${selected.quote}</h1>
-      <h3>${selected.source}</h3>
-    `;
-    return strPrint;
+  let numR = Math.floor(Math.random() * quotes.length);
+  let selected = quotes[numR];
+  return selected;
 };
-console.log(getRandomQuote())
-/***
- * `printQuote` function
-***/
-function printQuote(){
-getRandomQuote();
-//document.querySelector('load-quote').innerHTML = getRandomQuote();
-}
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
 
+
+//Run print function including call the getRandomQuote function after click on the button. Print output to the screen in HTML formats.
+function printQuote(){
+
+  //Call the get random quote function to get the object property and value.
+  let strPrint = getRandomQuote();
+
+  // Add quotes and sources object to printOut.
+  let printOut = `<p class='quote'>"${strPrint.quote}"</p><p class='source'>${strPrint.source}`
+
+  //Check to see if citation value is undefined/empty.If not, add it to printOut. Otherwise, don't print iundefined.
+  if(typeof strPrint.citation !== 'undefined'){
+  printOut +=  `<span class='citation'> ${strPrint.citation} </span>`;
+  };
+
+  //Check to see if year value is undefined/empty.If not, add it to printOut. Otherwise, don't print undefined.
+  if(typeof strPrint.year !== 'undefined'){
+  printOut +=  `<span class='year'> ${strPrint.year} </span>`;
+  };
+
+  printOut +=`</p>`;
+
+  // add the printOut object and then feed it to index.html's quote box element for screen print.
+  document.getElementById('quote-box').innerHTML = printOut;
+}
+
+//button function. Calls out the print quote function everytime the button is clicked.
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
